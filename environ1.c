c413e98e2bdb8da95edb699e8fa83c4dfa6f019e
Boloo66 <99683557+Boloo66@users.noreply.github.com>
2022-05-18 16:27:49 +0100

    Update environ1.c

--- environ1.c
+++ environ1.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * cmp_env_name - compares env variables names
+ * cmp_env_name - It compares env variables names
  * with the name passed.
  * @nenv: name of the environment variable
  * @name: name passed
  *
- * Return: 0 if are not equal. Another value if they are.
+ * Return: Return 0 if are not equal. Another value if they are.
  */
 int cmp_env_name(const char *nenv, const char *name)
 {

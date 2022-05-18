76bf4f9fc0f22e6eb9dd3f65f7c96e2c2b1e99f6
Boloo66 <99683557+Boloo66@users.noreply.github.com>
2022-05-18 16:30:01 +0100

    Update _mem.c

--- _mem.c
+++ _mem.c
@@ -6,7 +6,7 @@
  * @ptr: source pointer.
  * @size: size of the new pointer.
  *
- * Return: no return.
+ * Return: Returns no return.
  */
 void _memcpy(void *newptr, const void *ptr, unsigned int size)
 {
